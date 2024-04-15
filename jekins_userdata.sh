fdb682591e5d643a8bc08db3990a8c2e63e37c5c
Your Name <you@example.com>
2024-04-15 02:26:21 +0000

    a

--- jekins_userdata.sh
+++ jekins_userdata.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 sudo yum update -y
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
